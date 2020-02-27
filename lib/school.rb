a5c34205d325f592b195557d77698117c0e1c3f7
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-02-27 04:12:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,14 +9,14 @@ class School
   def add_student(student, level)
    roster[level] ||= []
    roster[level] << student
- end
+  end
 
  def grade(level)
    roster.detect do |x, y|
      if x == level
     return y
   end
-
+end
 
 
 end
